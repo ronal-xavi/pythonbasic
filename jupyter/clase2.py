713e2e49363cf38853beb14289ed4f16c58c882d
Ronal Mendoza <ronalrolando.mendozatipula@emeal.nttdata.com>
2023-03-30 11:01:03 -0500

    commit

--- jupyter/clase2.py
+++ jupyter/clase2.py
@@ -1,45 +1,45 @@
 ###################################################################
-#=========== PRIMERA FORMA DE RESOLVER LA ACTIVIDAD ==============
+# =========== PRIMERA FORMA DE RESOLVER LA ACTIVIDAD ==============
 ###################################################################
 
 import pandas as pd
 
-#Leer el archivo CSV con datos de notas
+# Leer el archivo CSV con datos de notas
 archivo = pd.read_csv("datos_csv.csv")
 
-#Agregar la columna promedio
-archivo["PROMEDIO"] = (archivo["NOTA1"] + archivo["NOTA2"] + archivo["NOTA3"]) / 3
-#Agregamos columas de aprobado y reprobado
-archivo.loc[archivo["PROMEDIO"]>10,"RESULTADO"] = "APROBADO"
-archivo.loc[archivo["PROMEDIO"]<=10,"RESULTADO"] = "REPROBADO"
+# Agregar la columna promedio
+archivo["PROMEDIO"] = (
+    archivo["NOTA1"] + archivo["NOTA2"] + archivo["NOTA3"]) / 3
+# Agregamos columas de aprobado y reprobado
+archivo.loc[archivo["PROMEDIO"] > 10, "RESULTADO"] = "APROBADO"
+archivo.loc[archivo["PROMEDIO"] <= 10, "RESULTADO"] = "REPROBADO"
 
 
-#Inprimir
+# Inprimir
 print(archivo)
 
-#Separando aprobados de reprobados
-aprobados = archivo[archivo["PROMEDIO"]>10]
-reprobados = archivo[archivo["PROMEDIO"]<=10]
+# Separando aprobados de reprobados
+aprobados = archivo[archivo["PROMEDIO"] > 10]
+reprobados = archivo[archivo["PROMEDIO"] <= 10]
 
-#Inprimir
+# Inprimir
 print(aprobados)
 print(reprobados)
 
-#Guardando en archivos separados
+# Guardando en archivos separados
 aprobados.to_csv("aprobados.csv")
 reprobados.to_csv("reprobados.csv")
 
 
-
 ###################################################################
-#=========== SEGUNDA FORMA DE RESOLVER LA ACTIVIDAD ==============
-#CON UN BUCLE EN BASE A LA COLUMNA RESULTADO
+# =========== SEGUNDA FORMA DE RESOLVER LA ACTIVIDAD ==============
+# CON UN BUCLE EN BASE A LA COLUMNA RESULTADO
 ###################################################################
 
 salida = archivo.RESULTADO.unique()
 print(salida)
 
 for i in archivo.RESULTADO.unique():
-    archivo2 = archivo[archivo["RESULTADO"]==i]
+    archivo2 = archivo[archivo["RESULTADO"] == i]
     print(archivo2)
     archivo2.to_csv(f"{i}.csv")
